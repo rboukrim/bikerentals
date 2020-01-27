6d5f86b121a58b196437866e283037d6d9aa01f9
rboukrim <rachid.boukrim@gmail.com>
2020-01-26 20:27:02 -0500

    moved json 'data' directory under services

--- src/services/Api.js
+++ src/services/Api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default() => {
   return axios.create({
-    baseURL: `../data`,
+    baseURL: `data`,
     withCredentials: false,
     headers: {
       'Accept': 'application/json',
